dd3def870a7a01040f823b030877b8795a73a55c
Karanjeet Singh <connect.karanjeet@gmail.com>
2017-03-25 11:34:46 -0700

    Added Youtube Explorer

---
+++ ytexplorer/src/main/java/edu/usc/csci599/Explorer.java
@@ -0,0 +1,22 @@
+package edu.usc.csci599;
+
+import edu.usc.csci599.util.Constants;
+
+/**
+ * Created by ksingh on 2/16/17.
+ */
+public class Explorer {
+
+    // Initialize Variables & Configuration
+    private void init() {
+        System.setProperty("webdriver.firefox.bin", Constants.FFX_BIN_PATH);
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
